a4b93cd9fb1fd3c36aae3821c8e14d5295391018
Parul Gupta <parulguptasmail@gmail.com>
2018-03-12 16:51:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
 
+def sort_array_asc(int_array)
+  sorted_array = int_array.sort
+end
+
+def sort_array_desc(int_array)
+  sorted_array = int_array.sort do |a,b|
+                  b<=>a
+                end
+end
+
+def sort_array_char_count(str_array)
+  sorted_array = str_array.sort do |a,b|
+                  a.length <=> b.length
+                end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  temp = array[index]
+  array[index] = array[destination_index]
+  array[destination_index] = temp
+  array
+end
+
+def reverse_array(array)
+  mid = array.length / 2
+  i = 0 
+  while i < mid
+    swap_elements_from_to(array,i,array.length - i - 1)
+    i += 1 
+  end
+  array
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |str|
+    arr = str.split("") 
+    arr[2] ='$'
+    new_array << arr.join
+  end
+  new_array
+end
+
+def start_with?(string, char)
+  arr = string.split("")
+  if arr[0] == char
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def find_a(array)
+  new_array = []
+  array.collect do |string|
+    new_array << string if start_with?(string,'a')
+  end
+ new_array
+end
+
+def sum_array(array)
+ sum = array.inject {|sum,num| sum+num}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element,index| 
+    if index != 1
+      element +'s'
+    else
+      element
+      end
+    end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,7 +29,12 @@ describe 'collections practice' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
-
+# Question 4 advanced
+  describe '#swap_elements_from_to' do
+    it 'swap the elements of an array from index to destination index' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+  end
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
